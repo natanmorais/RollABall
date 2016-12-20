e37d73f0db4d233e0be3b9f303eea8e90b8c5a04
natanmorais <morais_natan@hotmail.com>
2016-12-20 21:12:21 -0200

    Configurando apk corretamente e move com diversos swipes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,11 +14,14 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+
+		float moveHorizontal = Input.GetTouch (0).deltaPosition.x;
+		float moveVertical = Input.GetTouch (0).deltaPosition.y;
 	
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
+		rb.AddForce (movement / speed);
 	}
 }
