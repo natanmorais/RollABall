d1a3b68334255a827e949641fb87b46b2321c51f
natanmorais <morais_natan@hotmail.com>
2016-12-21 16:48:52 -0200

    Criado um inicio repetitivo que muda de cor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	private int placar = 0;
+	private GameObject[] itens = new GameObject[12];
+	public GameObject origem;
+	public Material oRed, oGreen;
 
 	private Rigidbody rb;
 
@@ -30,6 +34,19 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{
 			other.gameObject.SetActive (false);
+			itens [placar] = other.gameObject;
+			placar ++;
+		}
+		if (other.gameObject.CompareTag ("Origin") && placar == 12) {
+			placar = 0;
+			for (int i = 0; i < 12; i++) {
+				itens [i].SetActive (true);
+			}
+			origem.GetComponent<Renderer> ().material = oRed;
+		}
+		if (placar == 12) 
+		{
+			origem.GetComponent<Renderer> ().material = oGreen;
 		}
 	}
 }
