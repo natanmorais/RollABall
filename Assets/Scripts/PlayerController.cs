d80e37a20ec4f5adcd130f1c5574b579a444751c
natanmorais <morais_natan@hotmail.com>
2016-12-21 15:17:28 -0200

    Tutorial Finalizado + Swipe Arrumado
    Jogo praticamente pronto, não foi implementado parte de pontuação, controle no celular tá melhor, swipe funciona bem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,6 +22,14 @@ public class PlayerController : MonoBehaviour {
 	
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement / speed);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+		}
 	}
 }
