a05f4aa6a62e7ad7df1a54d508bbca1049b32683
natanmorais <morais_natan@hotmail.com>
2016-12-23 21:05:35 -0200

    Level 3 + Level 4 + Level 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	private int placar = 0;
-	private GameObject[] itens = new GameObject[12];
 	public GameObject origem;
 	public Material oRed, oGreen;
 
@@ -35,15 +34,9 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up") || other.gameObject.CompareTag ("Image"))
 		{
 			other.gameObject.SetActive (false);
-			itens [placar] = other.gameObject;
-			if(other.gameObject.CompareTag ("Pick Up"))placar ++;
+			//Descrever o método de ganho de moedas
 		}
-		if (other.gameObject.CompareTag ("Origin") && placar == 12) {
-			/*placar = 0;
-			for (int i = 0; i < 12; i++) {
-				itens [i].SetActive (true);
-			}
-			origem.GetComponent<Renderer> ().material = oRed;*/
+		if (other.gameObject.CompareTag ("Origin")) {
 			SceneManager.LoadScene("MenuBase",LoadSceneMode.Single);
 		}
 		if (placar == 12) 
